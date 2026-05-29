58ac3f145960cb4042aa7908a0f8dee87e836470
astper <35884105+astper21@users.noreply.github.com>
2026-05-29 14:49:15 +0800

    added the vercel envi
    added the vercel envi to not block on prod

--- backend/cozy_travel/settings.py
+++ backend/cozy_travel/settings.py
@@ -112,5 +112,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://localhost:3000',
+    'https://your-app.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
